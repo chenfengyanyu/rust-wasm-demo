6501e14a50824a88d2d5428b0dba6c816b2fa1a1
wangjiatao <wangjiatao@meituan.com>
2023-02-27 11:09:37 +0800

    feat: init project

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
